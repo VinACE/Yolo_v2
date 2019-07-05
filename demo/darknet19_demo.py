25f216f43249357b2e027382d042cabfbd82685f
vinace <vinsent.storage@gmail.com>
2019-07-05 18:27:34 +0530

    testing the model with own trained model

--- demo/darknet19_demo.py
+++ demo/darknet19_demo.py
@@ -21,7 +21,7 @@ def main():
 
     darknet19 = darknet.Darknet19(pretrained=True)
 
-    checkpopint = torch.load("/home/madhevan/Yolo_v2/ckpt_noHash_ep00010_loss103.5820_lr0.0001.pth.tar")
+    checkpoint = torch.load("/home/madhevan/Yolo_v2/ckpt_noHash_ep00010_loss103.5820_lr0.0001.pth.tar")
     try:
         checkpoint.eval()
     except AttributeError as error:
