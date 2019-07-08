00f8a1a24e115b7dbace218e75ecced62494a029
vinace <vinsent.storage@gmail.com>
2019-07-08 19:09:59 +0530

    testing nms output

--- test.py
+++ test.py
@@ -236,7 +236,8 @@ def nms(outputs,class_prob,class_threshold,num_class,img_size,anchor):
             # print(i) 
             final_bbox.append(sort_class_prob[0,:])
             
-              
+            print("final box count")
+            print(len(fina_bbox))
             print(final_bbox)     
     return final_bbox
     #output = 13 x 13 x 25 x n
